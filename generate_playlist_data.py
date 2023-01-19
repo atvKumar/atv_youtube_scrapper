d0fb09346c8083f31c5ebf79a026461d8aba4911
Kumaran <atv.kumar@gmail.com>
2023-01-19 12:36:16 +0530

    Added Video List Generation
    Added parsing for final_video.json generation from thisone.json. Also added duplicates that are fixed manually by hand.

---
+++ generate_playlist_data.py
@@ -0,0 +1,28 @@
+# -----------------------------------------------------------------------------
+# Generate Playlist Data Fixture to be imported into Django vod_backend APP
+# -----------------------------------------------------------------------------
+import json
+
+# Load the extracted and prepared data from extract_playlist_data.py
+with open("thisone.json", encoding="utf-8") as json_file:
+    data = json.load(json_file)
+
+playlist_data = []
+for index, playlist in enumerate(data, 1):
+    # print(data[playlist]["Name"])
+    # break
+    playlist_item = {"model": "vod.playlist", "pk": index}
+    playlist_field_data = {}
+    playlist_field_data["title"] = data[playlist]["Name"]
+    playlist_field_data["slug"] = data[playlist]["slug"]
+    playlist_field_data["short_description"] = data[playlist]["description"]
+    playlist_field_data["poster_image"] = f"playlist_thumbnail/{data[playlist]['slug']}.jpg"
+    playlist_field_data["external_url"] = data[playlist]["url"]
+    playlist_field_data["status"] = "Public"
+    playlist_field_data["language_id"] = "en"
+    playlist_item["fields"] = playlist_field_data
+    playlist_data.append(playlist_item)
+
+# print(playlist_data)
+with open("final_playlist.json", "w", encoding="utf-8") as outfile:
+    json.dump(playlist_data, outfile, ensure_ascii=False)
---
+++ generate_video_data.py
@@ -0,0 +1,50 @@
+# -----------------------------------------------------------------------------
+# Generate Video Data Fixture to be imported into Django vod_backend APP
+# -----------------------------------------------------------------------------
+import json
+from os.path import exists
+# Load the extracted and prepared data from extract_playlist_data.py
+with open("thisone.json", encoding="utf-8") as json_file:
+    data = json.load(json_file)
+
+result = []
+rest = []
+for index, playlist in enumerate(data, 1):
+    x = [video for video in data[playlist]["items"]]    
+    for i in x:
+        item = data[playlist]["items"][i]['Name']
+        if item not in result:
+            result.append(item)
+            rest.append(data[playlist]["items"][i])
+        # else:
+        #     print(item, "<<<<<<<<<<<<<<<<<<<")
+image_files = []
+print(len(rest))
+videos = []
+for x, a in enumerate(rest, 1):
+    # print(x, a)
+    # break
+    video_item = {"model": "vod.video", "pk": x}
+    video_field_data = {}
+    video_field_data["title"] = a["Name"]
+    video_field_data["slug"] = a["slug"]
+    video_field_data["type_id"] = 1
+    video_field_data["language_id"] = "en"
+    video_field_data["description"] = a["description"]
+    video_field_data["external_url"] = a["url"]
+    video_field_data["date_uploaded"] = a["date_uploaded"].replace('+00:00', 'Z')
+    video_field_data["view_count"] = 0
+    video_field_data["poster_image"] = f"video_thumbnail/{a['slug']}.jpg"
+    video_field_data["status"] = "Public"
+    video_field_data["post_activation"] = False
+    video_field_data["activation_date"] = None
+    video_item["fields"] = video_field_data
+    videos.append(video_item)
+
+    if video_field_data["poster_image"] not in image_files:
+        image_files.append(f"video_thumbnail/{a['slug']}.jpg")
+    else:
+        print("Duplicate images", video_field_data["poster_image"])
+
+with open("final_video.json", "w", encoding="utf-8") as outfile:
+    json.dump(videos, outfile, ensure_ascii=False)
